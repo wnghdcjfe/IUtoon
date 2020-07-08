e29aedd91fb36d946283e85b59e3857a02a270d3
pukuba <skatmddnjs0604@gmail.com>
2020-07-09 01:35:42 +0900

    error fiexd
    allAlbumSongList 쿼리 코드 간겨화
    allAlbumSongList 쿼리에서 앨범 사진 오류 발견후 해결완료

--- back-end/resolvers/Query.js
+++ back-end/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { ObjectID } = require('mongodb');
 const { BreakingChangeType } = require('graphql');
 const resizeImage = require('resize-image')
-const set = new Set(); 
+const set = new Set();
+let getName = "" 
 const change = (x) => {
     let see = String(x),a = "",b = "",cnt = 0
     for(let j=see.length-1; j>=0; j--){
@@ -36,14 +37,13 @@ const songDefault = ( collection ) => {
 }
 
 const albumDefault = ( collection ) => {
-    let names = ""
+    let names = "",reg = /\d+\집/g,b = []
     if(collection.id >= 100){
         const strArray = collection.albumInfo.split("")
         let index = strArray.findIndex((item,idx) => {return item === "글"})
         for(let i=index+2; i<strArray.length; i++) names+=strArray[i]
     }
-    let reg = /\d+\집/g,b = []
-    if(collection.id < 100) b = reg.exec(collection.albumInfo)
+    else b = reg.exec(collection.albumInfo)
     if(b[0] === null) return null
     let col_name = collection.id >= 100 ? names : b[0]
     if(set.has(col_name)) return null
@@ -54,6 +54,34 @@ const albumDefault = ( collection ) => {
         desc:collection.albumInfo,
         date:collection.date,
         img:imgLink
+    }
+    return ret
+}
+
+const albumSong = ( collection ) => {
+    let names = "",b = [], reg = /\d+\집/g,c="jpg"
+    if(collection.id > 99){
+        c="jepg"
+        const strArray = collection.albumInfo.split("")
+        let index = strArray.findIndex((item,idx) => {return item === "글"});
+        for(let k=index+2; k<strArray.length; k++) names += strArray[k];
+    }
+    else b = reg.exec(collection.albumInfo)
+    if(b[0] === null) return null
+    names = collection.id > 99 ? names : b[0]
+    if(names != getName) return null
+    names = collection.id > 99 ? names : b[0][0]
+    let ret = {
+        name:collection.title,
+        album:{
+            names:names,
+            desc:collection.albumInfo,
+            date:collection.date,
+            img:`http://localhost:12010/${names}.${c}`
+        },
+        date:collection.date,
+        id:collection.id,
+        img:setImgPath(11,20,"png")
     }
     return ret
 }
@@ -70,34 +98,8 @@ module.exports = {
     allSong: async (parent,args,{ db }) => db.collection('Song').find().toArray(),
     allAlbumSongList: async(parent,args,{ db }) => {
         const db_1 = await db.collection('Song').find().toArray()
-        let ret = []
-        for(let i=0; i<db_1.length; i++){
-            let arrname = ""
-            if(db_1[i].id>=100){
-                const strArray = db_1[i].albumInfo.split("");
-                let index = strArray.findIndex((item,idx) => {return item == "글"});
-                for(let k=index+2; k<strArray.length; k++) arrname += strArray[k];
-            }
-            let b = [],reg=""
-            if(i<99){
-                reg = /\d+\집/g
-                b = reg.exec(db_1[i].albumInfo)
-            }
-            if(b === null && db_1[i].id <= 99) continue;
-            if(b[0] == args.name || arrname == args.name) ret.push({
-                name:db_1[i].title,
-                album:{
-                    name:db_1[i].id >= 100 ? arrname : b[0],
-                    desc:db_1[i].albumInfo,
-                    date:db_1[i].date,
-                    img:"http://localhost:12010/" + args.name + ".jpeg"
-                },
-                date:db_1[i].date,
-                id:db_1[i].id,
-                img:setImgPath(11, 20, "png")
-            })
-        }
-        return ret
+        getName = args.name
+        return db_1.map(x => albumSong(x)).filter(e => e)
     },
     allAlbumList: async (parent,args,{ db }) => {
         const db_1 = await db.collection('Song').find().toArray();
